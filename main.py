8cb68a59c0f68b481ef0793e57831c7ddeca437c
pwsgoooo <pws_01@naver.com>
2024-08-03 22:00:39 +0900

    onlysuccessdbupdate

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from server.database.connection import Base,engine,SessionLocal
 from server.database.schemas import BinaryImage,DataList
+
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.mount('/client',StaticFiles(directory=str(Path(__file__).parent.absolute()/'client')), name='client')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 templates = Jinja2Templates(directory='client')
 
@@ -33,20 +44,28 @@ async def get_home(request:Request,db:Session=Depends(get_db)):
     return templates.TemplateResponse('home.html',{"request":request,"datas":datas})
 
 @app.get('/adddata')
-async def get_home(request:Request,db:Session=Depends(get_db)):
+async def get_home(request:Request, db:Session=Depends(get_db)):
     return templates.TemplateResponse('adddata.html',{"request":request})
 
-@app.post('/add')
+@app.post('/adddata')
 async def add_data(request:Request, db:Session=Depends(get_db),
                    title:str = Form(...), file:UploadFile = File(...)):
     try:
-        new_data = DataList(title= title, file= file.filename)
-        db.query(DataList).add(new_data)
+        new_data = DataList(title= title, favorite= file.filename)
+        db.add(new_data)
 
         new_img = BinaryImage(filename=file.filename, mimetype=file.content_type, content=await file.read())
-        db.query(BinaryImage).add(new_img)
-        
-        return HTMLResponse(status_code=302, headers= {"Location":'/'})
+        db.add(new_img)
+
+        db.commit()
+        db.refresh(DataList)
+        db.refresh(BinaryImage)
+
+
+        datas = db.query(BinaryImage).all()
+
+        return templates.TemplateResponse('home.html',{"request":request,"datas":datas})
+        # return HTMLResponse(status_code=302, headers= {"Location":'/'})
     
 
     except IntegrityError as e:
